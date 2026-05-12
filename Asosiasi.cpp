bf4ad77c284a2c2c78b9b5410d76276eb372e4a4
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-12 18:37:48 +0700

    menambahkan implementasi tambahDokter pada class pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -5,32 +5,26 @@ using namespace std;
 class dokter;
 class pasien { // asosiasi
 public:
-
     string nama;
     vector<dokter*> daftar_dokter;
-
     pasien(string pNama) :nama(pNama) {
         cout << "Pasien \"" << nama << "\" ada\n";
     }
-
     ~pasien() {
         cout << "Pasien \"" << nama << "\" tidak ada\n";
     }
-
     void tambahDokter(dokter*);
     void cetakDokter();
 };
 
 class dokter {
 public:
-
     string nama;
     vector<pasien*> daftar_pasien;
-
+    
     dokter(string pNama) :nama(pNama) {
         cout << "Dokter \"" << nama << "\" ada\n";
     }
-
     ~dokter() {
         cout << "Dokter \"" << nama << "\" tidak ada\n";
     }
@@ -38,3 +32,7 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
